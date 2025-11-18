e2a240a727f392fd32d69682b0d34ae948f81bb8
The-KE <kareemelayyan388@gmail.com>
2025-11-18 17:26:04 -0600

    Update and rename main.js?v2 to main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = prompt("put name ig");
+const space = document.createElement("p");
+let set = `Ur name is ${name}`;
+space.textContent = set;
+space.style.fontSize = "40px";
+space.style.fontFamily = "ui-rounded";
+document.body.append(space)

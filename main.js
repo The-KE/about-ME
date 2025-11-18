fad2391c46c5e251ea1da8958babd00ef9d3d922
The-KE <kareemelayyan388@gmail.com>
2025-11-18 17:06:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-const header = document.querySelector('.heading')
-header.textContent = 'Not'
+let name = prompt("put name ig")
+const space = document.createElement("p")
+let set = `Ur name is ${name}`
+space.innerHTML = `<p style='font-family:ui-rounded; font-size:40px'>${set}</p>`

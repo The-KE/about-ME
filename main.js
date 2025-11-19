973c624c60fbc0adeb190393e86189a1e1c21f94
The-KE <kareemelayyan388@gmail.com>
2025-11-18 18:28:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const head = document.querySelector('#top')
 function wow() {
   const e = document.createElement('p')
-  e.style.fontSize = '40px'
+  e.style.fontSize = '20px'
   e.style.color = '#FFFFFF'
   e.style.fontFamily = 'monospace'
   e.textContent = "Also known as The KE, UnknownKE(123), and Kareemsythah"
   head.append(e)
 }
-setTimeout(() => wow(), 1000)
+setTimeout(() => wow(), 3000)
